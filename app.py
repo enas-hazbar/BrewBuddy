cd94dd0f707e471eb586b9747cf7fed2dbaec874
danchikbananchik06 <63061597+danchikbananchik06@users.noreply.github.com>
2025-11-13 11:08:45 +0100

    nav bar and hero section

--- app.py
+++ app.py
@@ -13,11 +13,10 @@ def create_app():
 
     db.init_app(app)
 
-    # simple home route (optional)
+    # Home route
     @app.route("/")
     def home():
-        return "BrewBuddy is running!"
-
+        return render_template("home.html")
     return app
 
 # allow: python app.py to run the dev server
